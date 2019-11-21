3a8c5070791703e9f7656a4791a8979043ad9de9
Augusto Moreira <augustomoreira14@gmail.com>
2019-11-21 09:00:01 -0300

    Add heear /start

--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+$botman->hears('/start', function ($bot) {
+    $bot->reply('Olá! Tudo bem?');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
